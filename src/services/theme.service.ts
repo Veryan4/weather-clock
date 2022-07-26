f3da67b64058d433d297c55b7579d8ef922a2810
Veryan Goodship <veryan@pivohub.com>
2022-07-26 13:59:25 -0400

    fix themeing

--- src/services/theme.service.ts
+++ src/services/theme.service.ts
@@ -5,13 +5,6 @@ export const themeService = { getTheme, changeTheme, THEME_EVENT };
 
 const root = document.querySelector(":root") as HTMLElement;
 
-let theme = getTheme();
-if (theme === "light") {
-  setLightTheme();
-} else {
-  setDarkTheme();
-}
-
 function setLightTheme(): void {
   root.style.setProperty("--main-bg-color", "#fff");
   root.style.setProperty("--main-text-color", "#888888");
@@ -30,28 +23,11 @@ function getTheme(): string {
   return storedTheme ? storedTheme : "light";
 }
 
-function changeTheme(newTheme?: "light" | "dark"): void {
-  if(newTheme) {
-    if (theme == newTheme) {
-      return
-    }
-    theme = newTheme
-    if (theme === "light") {
-      theme = "light";
-      setLightTheme();
-    } else {
-      theme = "dark";
-      setDarkTheme();
-      
-    }
+function changeTheme(theme: "light" | "dark"): void {
+  if (theme === "light") {
+    setLightTheme();
   } else {
-    if (theme === "light") {
-      theme = "dark";
-      setDarkTheme();
-    } else {
-      theme = "light";
-      setLightTheme();
-    }
+    setDarkTheme();
   }
   localStorage.setItem(THEME_KEY, theme);
   window.dispatchEvent(new CustomEvent(THEME_EVENT));
